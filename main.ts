611ccce093b033ffca3738ac44b2f11b280ac80b
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2022-01-02 15:51:30 +0000

    Updated to follow an object

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 let distance = 0
 basic.forever(function () {
+    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
     distance = Kitronik_Move_Motor.measure()
-    basic.showNumber(distance)
+    if (distance > 10) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 100)
+    }
 })
