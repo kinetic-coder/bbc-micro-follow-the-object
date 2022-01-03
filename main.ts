7b57fdd369ad421e2acc44fec9c780c93e23850e
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2022-01-03 13:47:48 +0000

    Updated to stop if <10 cm

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 let distance = 0
+function showStopLights () {
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    moveMotorZIP.show()
+}
+function showGoLights () {
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    moveMotorZIP.show()
+}
 basic.forever(function () {
-    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+    Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Left, 15)
     distance = Kitronik_Move_Motor.measure()
     if (distance > 10) {
+        showGoLights()
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 100)
+    } else {
+        showStopLights()
+        Kitronik_Move_Motor.stop()
     }
 })
