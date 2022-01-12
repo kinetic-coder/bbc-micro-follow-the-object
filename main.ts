d9ada02fb5e8dc0a8a18c6ec55e23db071e8e9aa
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2022-01-12 21:26:50 +0000

    Updated to follow or move away

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
-let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
-let distance = 0
+function showBackwardsLights () {
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+    moveMotorZIP.show()
+}
 function showStopLights () {
     moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
     moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    moveMotorZIP.show()
+}
+function showGetReadyLights () {
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
     moveMotorZIP.show()
 }
 function showGoLights () {
@@ -10,12 +18,19 @@ function showGoLights () {
     moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
     moveMotorZIP.show()
 }
+let distance = 0
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
+showGetReadyLights()
+basic.pause(2000)
 basic.forever(function () {
     Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Left, 15)
     distance = Kitronik_Move_Motor.measure()
-    if (distance > 10) {
+    if (distance > 15) {
         showGoLights()
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 100)
+    } else if (distance < 5) {
+        showBackwardsLights()
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
     } else {
         showStopLights()
         Kitronik_Move_Motor.stop()
